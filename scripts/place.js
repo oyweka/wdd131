25e4d096c335663762fc1cf08933fe39817f2078
oyweka <alexoyweka@gmail.com>
2026-09-25 18:40:45 +0300

    week 03 done

---
+++ scripts/place.js
@@ -0,0 +1,17 @@
+const year = new Date().getFullYear();
+document.querySelector("#currentyear").textContent = year;
+
+const lastModified = document.lastModified;
+document.querySelector("#lastModified").textContent = `Last Modified: ${lastModified}`;
+
+const temperature = 18;
+const windSpeed = 10;
+function calculateWindChill(temperature, windSpeed) {
+    return 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
+}
+if (temperature <= 10 && windSpeed > 4.8) {
+    document.querySelector("#windchill").textContent =
+        `${calculateWindChill(temperature, windSpeed).toFixed(1)} °C`;
+} else {
+    document.querySelector("#windchill").textContent = "N/A";
+}
